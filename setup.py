9a3782a68c80ae9636bee1f8d396873ef54548b9
financial <financialfly@outlook.com>
2019-05-16 12:45:54 +0800

    update des

---
+++ setup.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='async-request',
+    version=0.12,
+    description=(
+        'A lightweight network request framework'
+    ),
+    long_description=open('README.md').read(),
+    author='financial',
+    author_email='1012593988@qq.com',
+    maintainer='financial',
+    maintainer_email='1012593988@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/financialfly/async-request',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+            'requests>=2.14.0'
+        ]
+)
