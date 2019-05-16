ecfe2b26e01fe1b60325d4f525d6dd6b73b808cf
financial <financialfly@outlook.com>
2019-05-16 09:46:42 +0800

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+from async_request import Request, Crawler
+
+def parse_baidu(response):
+    print(response.url, response.status_code)
+    yield Request('https://cn.bing.com/', callback=parse_bing)
+
+def parse_bing(response):
+    print(response.url, response.status_code)
+    yield Request('https://github.com/financialfly/async-request', callback=parse_github)
+
+def parse_github(response):
+    print(response.url, response.status_code)
+    yield {'hello': 'github'}
+
+def process_result(result):
+    print(result)
+
+c = Crawler([Request(url='https://www.baidu.com', callback=parse_baidu)], result_callback=process_result)
+c.run()
+c.stop()
