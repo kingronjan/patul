e52e7eeb7f4bd73211d26d298927e89d9c68f9fa
financial <financialfly@outlook.com>
2019-05-16 15:12:02 +0800

    xpath selector

---
+++ async_request/xpath.py
@@ -0,0 +1,21 @@
+from lxml import etree
+
+class XpathSelector(object):
+
+    def __init__(self, raw_text=None):
+        self.html = None
+        self._text = raw_text
+
+    def get(self):
+        try:
+            return self.html.xpath(self.syntax)[0]
+        except IndexError:
+            return None
+
+    def getall(self):
+        return self.html.xpath(self.syntax)
+
+    def __call__(self, syntax):
+        self.syntax = syntax
+        self.html = etree.HTML((self._text))
+        return self
