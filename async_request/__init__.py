3353605debac453666c9dbf14bb99526ff00cd63
financial <financialfly@outlook.com>
2019-05-17 09:34:48 +0800

    add crawl function

--- async_request/__init__.py
+++ async_request/__init__.py
@@ -1,2 +1,6 @@
 from .crawler import Crawler
 from .request import Request, FormRquest
+
+def crawl(requests, result_callback=None, close_eventloop=True):
+    c = Crawler(requests=requests, result_callback=result_callback)
+    c.run(close_eventloop=close_eventloop)
