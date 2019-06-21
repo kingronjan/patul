00d81d5b251164f739d51f10145a80563d0ec64c
financial <financialfly@outlook.com>
2019-06-21 11:58:14 +0800

    add: new test function

--- async_request/__init__.py
+++ async_request/__init__.py
@@ -4,3 +4,11 @@ from .request import Request, FormRquest
 def crawl(requests, result_callback=None, close_eventloop=True):
     c = Crawler(requests=requests, result_callback=result_callback)
     c.run(close_eventloop=close_eventloop)
+
+def test(url, req_cls=Request, **req_kw):
+    def test(func):
+        req = req_cls(url, callback=func, **req_kw)
+        if req_kw.get('callback'):
+            raise TypeError('Cant assign the callback argument to test decorator.')
+        crawl([req])
+    return test
