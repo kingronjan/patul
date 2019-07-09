fc8e1f5ff90013870796edb91b01f46f67b4c09d
financial <financialfly@outlook.com>
2019-07-09 11:07:21 +0800

    fix: encoding error to content

--- async_request/response.py
+++ async_request/response.py
@@ -2,31 +2,44 @@ import re
 from async_request.xpath import Xpath
 from urllib.parse import urljoin
 
+_default_coding = 'utf-8'
+
 class Response(object):
 
     def __init__(self, response, request):
+        self._response = response
         self.content = response.content
         self.status = response.status_code
         self.url = response.url
         self.request = request
-        self._set_text()
-        self._set_xpath()
-        del response
+
+    def __getattr__(self, item):
+        return getattr(self._response, item)
 
     @property
     def meta(self):
         return self.request.meta
 
-    def _set_text(self):
-        charset = re.search(b'charset=(.*?)"', self.content)
+    @property
+    def text(self):
+        if hasattr(self, '_text'):
+            return self._text
+        charset = re.search(b'charset=(.+?)"', self.content)
         if charset:
             charset = charset.group(1).decode('utf-8')
-            self.text = self.content.decode(charset)
+            try:
+                text = self.content.decode(charset)
+            except:
+                text = self.content.decode(_default_coding)
         else:
-            self.text = self.content.decode('utf-8')
+            text = self.content.decode(_default_coding)
+        self._text = text
+        return self._text
 
-    def _set_xpath(self):
-        self.xpath = Xpath(self.text)
+    def xpath(self, syntax):
+        if not hasattr(self, '_xpath_selector'):
+            self._xpath_selector = Xpath(self.text)
+        return self._xpath_selector(syntax)
 
     def urljoin(self, url):
         return urljoin(self.url, url)
