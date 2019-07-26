5ba701761a60e6c65277f3a327f7ed3de6a6234e
financial <financialfly@outlook.com>
2019-07-26 09:55:00 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='async-request',
-    version=0.161,
+    version=0.1612,
     description=(
         'A lightweight network request framework'
     ),
