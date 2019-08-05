31bfbf4e07dbd094454d7bdf59d7931666b07dea
financial <financialfly@outlook.com>
2019-08-05 14:56:18 +0800

    update: test

--- async_request/test.py
+++ async_request/test.py
@@ -1,15 +1,15 @@
-import async_request as ar
+from async_request import Request, crawl, fetch
 
 
 def parse_baidu(response):
     print(response.url, response.status_code)
-    yield ar.Request('https://cn.bing.com/', callback=parse_bing)
+    yield Request('https://cn.bing.com/', callback=parse_bing)
 
 
 def parse_bing(response):
     print(response.url, response.status_code)
     print(response.xpath('//a/@href').get())
-    yield ar.Request('https://github.com/financialfly/async-request', callback=parse_github)
+    yield Request('https://github.com/financialfly/async-request', callback=parse_github)
 
 
 def parse_github(response):
@@ -21,15 +21,14 @@ def process_result(result):
     print(result)
 
 
+def parse():
+    response = fetch('https://www.bing.com')
+    response2 = fetch('https://www.baidu.com')
+    print(response, response2)
+
+
 if __name__ == '__main__':
-    # request_list = [ar.Request(url='https://www.baidu.com', callback=parse_baidu)]
-    # ar.crawl(request_list, result_callback=process_result)
-
-    from async_request import test
-
-    @test('https://www.baidu.com')
-    def parse(response):
-        print(response.url)
-        print(response.xpath('//a/@href').getall())
+    # request_list = [Request(url='https://www.baidu.com', callback=parse_baidu)]
+    # crawl(request_list, result_callback=process_result)
 
     parse()
