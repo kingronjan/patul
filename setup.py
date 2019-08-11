564125d6c7b96673624674396312399580dc8cc2
Hifinancial <financial@github.com>
2019-08-11 11:35:58 +0800

    update to 1616

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='async-request',
-    version=0.1615,
+    version=0.1616,
     description=(
         'A lightweight network request framework'
     ),
