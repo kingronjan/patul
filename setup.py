0887791715ad97f38c74599402741135ecaa9c48
Hifinancial <financial@github.com>
2020-02-27 23:22:47 +0800

    updated.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='async-request',
-    version=0.18,
+    version=0.181,
     description=(
         'A lightweight network request framework'
     ),
