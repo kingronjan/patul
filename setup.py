4bd9f5421b3ac8b55566beda3bb61fa0e163d97a
Hifinancial <financial@github.com>
2020-04-05 22:30:41 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='patul',
-    version=0.1002,
+    version=0.1003,
     description=(
         'A tiny spider based on asyncio and requests.'
     ),
